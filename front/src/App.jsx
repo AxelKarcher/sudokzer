e3a642f25fb7432d2f87fb8f784a5fc3eb86dad3
Axel Karcher <axel.karcher@epitech.eu>
2023-03-01 13:22:09 +0200

    [FIX] - True indexs of cells, refacto handleCellClick function

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,45 +1,57 @@
-import {useState} from 'react'
-import axios from 'axios'
+import {useState, useEffect} from 'react'
+import {makepuzzle} from 'sudoku'
 
 import './App.scss'
 
-const numbers = [
-	{label: '1', value: '1'},
-	{label: '2', value: '2'},
-	{label: '3', value: '3'},
-	{label: '4', value: '4'},
-	{label: '5', value: '5'},
-	{label: '6', value: '6'},
-	{label: '7', value: '7'},
-	{label: '8', value: '8'},
-	{label: '9', value: '9'},
-	{label: 'X', value: '0'}
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+const bIdx = [
+	[0, 1, 2, 9, 10, 11, 18, 19, 20],
+	[3, 4, 5, 12, 13, 14, 21, 22, 23],
+	[6, 7, 8, 15, 16, 17, 24, 25, 26],
+	[27, 28, 29, 36, 37, 38, 45, 46, 47],
+	[30, 31, 32, 39, 40, 41, 48, 49, 50],
+	[33, 34, 35, 42, 43, 44, 51, 52, 53],
+	[54, 55, 56, 63, 64, 65, 72, 73, 74],
+	[57, 58, 59, 66, 67, 68, 75, 76, 77],
+	[60, 61, 62, 69, 70, 71, 78, 79, 80]
 ]
 
 const App = () => {
 
-  const [grid, setGrid] = useState(Array(81).fill({content: [], isCandidate: false}))
+  const [grid, setGrid] = useState(Array(81).fill())
   const [currNb, setCurrNb] = useState('0')
   const [isCandidate, setIsCandidate] = useState(false)
+
+	useEffect(() => {
+		let puzzle = makepuzzle()
+
+		setGrid(puzzle?.map((elem) => {
+			if (!elem) {
+				return {content: [], isCandidate: false}
+			} else {
+				return {content: [elem?.toString()]}
+			}
+		}))
+	}, [])
 
   const handleCellClick = (i) => {
 		setGrid((old) =>
 			old.map((elem, idx) => {
-				if (idx === i) {
+				if (idx === i && elem?.isCandidate !== undefined) {
+					let newContent
+
 					if (currNb === '0') {
-						return {...elem, content: []}
-					}
-					if (isCandidate && elem?.isCandidate) {
+						newContent = []
+					} else if (isCandidate && elem?.isCandidate) {
 						if (elem?.content?.includes(currNb)) {
-							return {...elem, content: elem?.content?.filter((nb) => {
-								return nb !== currNb
-							})}
+							newContent = elem?.content?.filter((nb) => {return nb !== currNb})
 						} else {
-							return {...elem, content: elem?.content?.concat(currNb)?.sort()}
+							newContent = elem?.content?.concat(currNb)?.sort()
 						}
-					} else {
-						return {...elem, content: [currNb], isCandidate: isCandidate}
-					}
+					} else {newContent = [currNb]}
+
+					return {...elem, content: newContent, isCandidate: isCandidate}
 				}
 				return elem
 			}
@@ -51,13 +63,19 @@ const App = () => {
       <div id='grid' className='panel-base'>
 				{Array(9).fill()?.map((_elem1, i1) => (
 					<div className='block' key={i1} style={{borderRadius: 16}}>
-						{grid?.slice(i1 * 9, i1 * 9 + 9)?.map((elem2, i2) => (
+						{bIdx[i1]?.map((idx, i2) => (
 							<div
 								key={i2}
-								className={'cell' + (elem2?.isCandidate ? ' candidates' : '')}
-								onClick={() => handleCellClick(i2 + i1 * 9)}
+								className={
+									'cell' +
+									(grid[idx]?.isCandidate ? ' candidates' : '') +
+									(grid[idx]?.isCandidate === undefined ? ' default' : '')
+								}
+								onClick={() => handleCellClick(idx)}
 							>
-								{elem2?.content?.map((elem2, i2) => (<div key={i2}>{elem2}</div>))}
+								{grid[idx]?.content?.map((digit, i3) => (
+									<div key={i3}>{digit}</div>))
+								}
 							</div>
 						))}
 					</div>
@@ -68,13 +86,10 @@ const App = () => {
 					{numbers?.map((elem, i) => (
 						<div
 							key={i}
-							className='cell'
-							onClick={() => setCurrNb(elem?.value)}
-							style={{backgroundColor: elem?.value === currNb ? 'black' : 'white',
-								color: elem?.value === currNb ? 'white' : 'black'
-							}}
+							className={'cell' + (elem === currNb ? ' selected' : '')}
+							onClick={() => setCurrNb(elem)}
 						>
-							{elem?.label}
+							{elem === '0' ? 'X' : elem}
 						</div>
 					))}
 				</div>
